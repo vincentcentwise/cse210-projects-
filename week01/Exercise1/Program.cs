c43e1a1ffaacee3f1ae2ba0423c707e569fcd9d8
vincentcentwise <balogun_vincent@yahoo.com>
2025-11-04 09:51:08 +0100

    excersie 2 done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
-        Console.Write("What is your last name? ");
+        Console.WriteLine("What is your first name? ");
+        Console.WriteLine("What is your last name? ");
         string firstName = Console.ReadLine();
         string LastName = Console.ReadLine();
         Console.WriteLine($"Your name is {firstName}, {LastName}");
-        
+
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your score: ");
+        int score = int.Parse(Console.ReadLine());
+
+       
+         int A_GRADE = 90;
+         int B_GRADE = 80;
+         int C_GRADE = 70;
+         int D_GRADE = 60;
+        string grade
+;
+        if (score >= A_GRADE)
+        {
+            Console.WriteLine("Score is greater than or equal to A grade threshold.");
+            Console.WriteLine("Congratulations! you got an A grade");
+            grade = "A";
+        }
+
+        else if (score >= B_GRADE)
+        {
+            Console.WriteLine("Score is greater than or equal to B grade threshold.");
+            Console.WriteLine("Well done! you got a B grade, Therefore you passed the course");
+            grade = "B";
+        }
+
+        else if (score >= C_GRADE)
+        {
+            Console.WriteLine("Score is greater than or equal to C grade threshold.");
+            Console.WriteLine("Good effort! you got a C grade, Therefore you passed the course");
+            grade = "C";
+        }
+        else if (score >= D_GRADE)
+        {
+            Console.WriteLine("Score is greater than or equal to D grade threshold.");
+            Console.WriteLine("You got a D grade, Therefore you failed the course");
+            Console.WriteLine("Better luck next time!");
+            grade = "D";
+        }
+        else
+        {
+            Console.WriteLine("Score is less than D grade threshold.");
+            Console.WriteLine("Unfortunately, you got an F grade, Therefore you failed the course");
+            Console.WriteLine("Better luck next time!");
+            grade = "F";
+        }
+
+        Console.WriteLine($"Score: {score} -> Grade: {grade}");
     }
 }
